34123f124f9d9ec3aa1e112caf64778419d64339
Alexandr Berbat <aleksandrberbat@gmail.com>
2019-12-12 19:15:50 +0200

    Update index.js
    add html/js file with lab4

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const PORT = process.env.PORT || 3000;
 const http = require('http')
-const hostname = '127.0.0.1'
-const port = 3000
-const server = http.createServer((req, res) => {
- res.statusCode = 200
- res.setHeader('Content-Type', 'text/plain')
- res.end('Hi there!\n')
+const fs = require("fs");
+const server = http.createServer((req, response) => {
+ response.statusCode = 200
+ response.setHeader("Content-Type", "text/html")
+ fs.readFile("laba12.html", null, (error, data) => {
+  if (error) {
+    response.writeHead(404);
+    response.write("Whoops! File not found!");
+  } else {
+    response.write(data);
+  }
+  response.end();
+  });
 })
-server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`)
+server.listen(PORT, () => {
+ console.log(`Server running`)
 })
